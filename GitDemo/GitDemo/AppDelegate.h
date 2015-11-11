5d76a3d4fe33d058f18dd92b8a57a68179a19b09
CDP <389926971@qq.com>
2015-11-11 15:10:04 +0800

    first init

---
+++ GitDemo/GitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitDemo
+//
+//  Created by qianfeng on 15/11/11.
+//  Copyright © 2015年 DaoPanCui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitDemo/GitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitDemo
+//
+//  Created by qianfeng on 15/11/11.
+//  Copyright © 2015年 DaoPanCui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
